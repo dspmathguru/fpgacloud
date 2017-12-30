3f89063a4e4becf61d82c9b57b7441e8acca2294
Richard J. Tobias <dspmathguru@gmail.com>
2017-12-29 20:59:15 -0800

    use correct python

--- aws-setup.sh
+++ aws-setup.sh
@@ -10,7 +10,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 mkdir .venv
-python -m venv .venv
+python3.6 -m venv .venv
 source .venv/bin/activate
 
 pip install -r requirements.txt
