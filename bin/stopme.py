242fff36f94c8807d93da588862d7222dcc44833
Richard J. Tobias <dspmathguru@gmail.com>
2017-12-30 23:14:08 +0000

    shouls stop current instance logged into

--- bin/stopme.py
+++ bin/stopme.py
@@ -2,8 +2,12 @@
 # Stop the instance that this command is run in
 #    Assumes you have permission through a role to do this
 
-import json
+import boto3
 import MyMetaData
 
 if __name__ == "__main__":
-  myMetaData = MyMetaData.MyMetaData()
+  myMetaData = MyMetaData.MyMetaData().meta
+  zone = myMetaData['placement']['availability-zone']
+  print(zone)
+  ec2 = boto3.resource('ec2', region_name=zone)
+  ec2.instances.filter(InstanceId=myMetaData['instance-id']).stop()
