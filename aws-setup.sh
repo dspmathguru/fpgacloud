09e2ed7618393a52e1988560af119cfe3fc9d3ee
Richard J. Tobias <dspmathguru@gmail.com>
2017-12-30 10:38:13 -0800

    added fpgacloud/.venv/bin to path

--- aws-setup.sh
+++ aws-setup.sh
@@ -2,13 +2,15 @@
 # Setup for AWS Amazon Linux environments
 
 sudo yum install python36-devel python36-pip -y
-sudo yum install direnv
+
 
 # get the directory of this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
+# a difficult way to get .venv created as there is some issue on Amazon Linux
+#   with module venv
 python3.6 -m venv --without-pip .venv
 source .venv/bin/activate
 curl https://bootstrap.pypa.io/get-pip.py | python
@@ -16,3 +18,8 @@ deactivate
 source .venv/bin/activate
 
 pip install -r requirements.txt
+
+# should add check if this was already added
+echo \\
+  "\n### auto added by fpgacloud/aws-setup.sh\nexport PATH=$PATH:~/fpgacloud/.venv/bin" \\
+  >> ~/.basrc
