eb38d4d81b4a9cecf4c4eb0a349d3046c1afa587
Richard J. Tobias <dspmathguru@gmail.com>
2017-12-30 21:06:15 -0800

    moving to Python project style directory structure

--- bin/stopme.py
+++ bin/stopme.py
@@ -4,10 +4,20 @@
 # Basically a fancy way of saying 'sudo shutdown -h now'
 
 import boto3
-import MyMetaData
+
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from fpgacloud import MyMetaData
 
 if __name__ == "__main__":
-  myMetaData = MyMetaData.MyMetaData().meta
+  myMetaData = MyMetaData().meta
   region_name = myMetaData['placement']['availability-zone'][:-1]
   print(region_name)
   ec2 = boto3.resource('ec2', region_name=region_name)
